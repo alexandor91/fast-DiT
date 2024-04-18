626a3084678012d97283a90d77551d1de815bcc8
kangxueyagn@126.com <kangxueyagn@126.com>
2024-04-18 22:21:44 +1000

    dino feature added, along with the tsne viz tool for verification

--- models.py
+++ models.py
@@ -14,7 +14,17 @@ import torch.nn as nn
 import numpy as np
 import math
 from timm.models.vision_transformer import PatchEmbed, Attention, Mlp
+# from dinov2.models.vision_transformer import vit_large 
+from PIL import Image 
+import torchvision.transforms as transforms 
+import os
+import einops as E
+import torch.nn.functional as F
+from PIL import Image 
+import torchvision.transforms as transforms
+from sklearn.manifold import TSNE
 
+# from dinov2.models import dinov2
 
 def modulate(x, shift, scale):
     return x * (1 + scale.unsqueeze(1)) + shift.unsqueeze(1)
@@ -178,6 +188,13 @@ class DiT(nn.Module):
         ])
         self.final_layer = FinalLayer(hidden_size, patch_size, self.out_channels)
         self.initialize_weights()
+        # self.semantic_model = vit_large(
+        #     patch_size=14,
+        #     img_size=526,
+        #     init_values=1.0,
+        #     block_chunks=0
+        # )
+        # self.semantic_model.load_state_dict(torch.load('dinov2_vitl14_pretrain.pth'))
 
     def initialize_weights(self):
         # Initialize transformer layers:
@@ -374,3 +391,233 @@ DiT_models = {
     'DiT-B/2':  DiT_B_2,   'DiT-B/4':  DiT_B_4,   'DiT-B/8':  DiT_B_8,
     'DiT-S/2':  DiT_S_2,   'DiT-S/4':  DiT_S_4,   'DiT-S/8':  DiT_S_8,
 }
+
+
+def center_padding(images, patch_size):
+    _, _, h, w = images.shape
+    diff_h = h % patch_size
+    diff_w = w % patch_size
+
+    if diff_h == 0 and diff_w == 0:
+        return images
+
+    pad_h = patch_size - diff_h
+    pad_w = patch_size - diff_w
+
+    pad_t = pad_h // 2
+    pad_l = pad_w // 2
+    pad_r = pad_w - pad_l
+    pad_b = pad_h - pad_t
+
+    images = F.pad(images, (pad_l, pad_r, pad_t, pad_b))
+    return images
+
+def tokens_to_output(output_type, dense_tokens, cls_token, feat_hw):
+    if output_type == "cls":
+        assert cls_token is not None
+        output = cls_token
+    elif output_type == "gap":
+        output = dense_tokens.mean(dim=1)
+    elif output_type == "dense":
+        h, w = feat_hw
+        dense_tokens = E.rearrange(dense_tokens, "b (h w) c -> b c h w", h=h, w=w)
+        output = dense_tokens.contiguous()
+    elif output_type == "dense-cls":
+        assert cls_token is not None
+        h, w = feat_hw
+        dense_tokens = E.rearrange(dense_tokens, "b (h w) c -> b c h w", h=h, w=w)
+        cls_token = cls_token[:, :, None, None].repeat(1, 1, h, w)
+        output = torch.cat((dense_tokens, cls_token), dim=1).contiguous()
+    else:
+        raise ValueError()
+
+    return output
+
+class DINO(torch.nn.Module):
+    def __init__(
+        self,
+        dino_name="dino",
+        model_name="vitb16",
+        output="dense",
+        layer=-1,
+        return_multilayer=False,
+    ):
+        super().__init__()
+        feat_dims = {
+            "vitb8": 768,
+            "vitb16": 768,
+            "vitb14": 768,
+            "vitb14_reg": 768,
+            "vitl14": 1024,
+            "vitg14": 1536,
+        }
+
+        # get model
+        self.model_name = dino_name
+        self.checkpoint_name = f"{dino_name}_{model_name}"
+        dino_vit = torch.hub.load(f"facebookresearch/{dino_name}", self.checkpoint_name)
+        self.vit = dino_vit.eval().to(torch.float32)
+        self.has_registers = "_reg" in model_name
+
+        assert output in ["cls", "gap", "dense", "dense-cls"]
+        self.output = output
+        self.patch_size = self.vit.patch_embed.proj.kernel_size[0]
+
+        feat_dim = feat_dims[model_name]
+        feat_dim = feat_dim * 2 if output == "dense-cls" else feat_dim
+
+        num_layers = len(self.vit.blocks)
+        multilayers = [
+            num_layers // 4 - 1,
+            num_layers // 2 - 1,
+            num_layers // 4 * 3 - 1,
+            num_layers - 1,
+        ]
+
+        if return_multilayer:
+            self.feat_dim = [feat_dim, feat_dim, feat_dim, feat_dim]
+            self.multilayers = multilayers
+        else:
+            self.feat_dim = feat_dim
+            layer = multilayers[-1] if layer == -1 else layer
+            self.multilayers = [layer]
+
+        # define layer name (for logging)
+        self.layer = "-".join(str(_x) for _x in self.multilayers)
+
+    def forward(self, images):
+
+        # pad images (if needed) to ensure it matches patch_size
+        images = center_padding(images, self.patch_size)
+        h, w = images.shape[-2:]
+        h, w = h // self.patch_size, w // self.patch_size
+
+        if self.model_name == "dinov2":
+            x = self.vit.prepare_tokens_with_masks(images, None)
+        else:
+            x = self.vit.prepare_tokens(images)
+
+        embeds = []
+        for i, blk in enumerate(self.vit.blocks):
+            x = blk(x)
+            if i in self.multilayers:
+                embeds.append(x)
+                if len(embeds) == len(self.multilayers):
+                    break
+
+        num_spatial = h * w
+        outputs = []
+        for i, x_i in enumerate(embeds):
+            cls_tok = x_i[:, 0]
+            # ignoring register tokens
+            spatial = x_i[:, -1 * num_spatial :]
+            x_i = tokens_to_output(self.output, spatial, cls_tok, (h, w))
+            outputs.append(x_i)
+
+        return outputs[0] if len(outputs) == 1 else outputs
+    
+def save_tensor_as_image(tensor, filename):
+    # Normalize the tensor to [0, 1] range
+    tensor = tensor.squeeze(0)  # Remove the batch dimension
+    tensor = tensor.permute(1, 2, 0)  # Rearrange channels to (H, W, C)
+    tensor_min = -10.0 #tensor.min()
+    tensor_max = 10.0 #tensor.max()
+
+
+    # Convert the flattened tensor to a NumPy array
+    numpy_tensor = tensor.detach().cpu().numpy()
+    flattened_data = numpy_tensor.reshape((-1, numpy_tensor.shape[2]))
+
+    # Apply t-SNE (adjust hyperparameters as needed)
+    tsne = TSNE(n_components=3, perplexity=30, random_state=42)
+    embedded_data = tsne.fit_transform(flattened_data)
+    # Apply t-SNE
+    print('#######tsne used########')
+    print(embedded_data.shape)
+
+    # Reshape for color image (assuming 3 channels for RGB)
+    if embedded_data.shape[1] < 2:
+        raise ValueError("Embedded data requires at least 3 dimensions (RGB) for color image")
+
+    # Convert the reduced NumPy array back to a PyTorch tensor
+    tensor = torch.from_numpy(embedded_data.T).view(-1, 32, 32).to(device)  # Shape: (1, 3/756, 32, 32)
+
+    # # Convert the reduced tensor to a PIL Image
+    # reduced_tensor = reduced_tensor.squeeze(0).permute(1, 2, 0)  # Shape: (32, 32, 3)
+    # reduced_tensor = reduced_tensor.clamp(0, 1)  # Clamp values to [0, 1] range
+    
+    tensor = (tensor - tensor_min) / (tensor_max - tensor_min)  # Normalize to [0, 1]
+    print("######save tensor to iamage for viz##########")
+    print(tensor.shape)
+    # Scale the tensor to [0, 255] range and convert to uint8
+    tensor = tensor.mul(255).clamp(0, 255).byte()  # Multiply by 255 and clamp to [0, 255]
+    if tensor.shape[2] > 4:
+        tensor = tensor[:, : , 1:-1]
+    # Resize the tensor to 256x256
+    transform = transforms.Resize((256, 256), antialias=True)
+    tensor = transform(tensor)
+
+    # Convert the tensor to a PIL Image
+    img = transforms.ToPILImage()(tensor)
+
+    # Save the image
+    img.save(filename)
+
+if __name__ == "__main__":
+    # Download all DiT checkpoints
+    print("######main starts#########")
+    # semantic_model = vit_large(
+    #     patch_size=14,
+    #     img_size=526,
+    #     init_values=1.0,
+    #     block_chunks=0
+    # )
+    base_dir = '/media/diffusion'
+    folder_type = '0a5c013435/sampled_feature_256'
+    img_folder_type = 'rgb'
+    filename = 'frame_000440.jpg'
+    vae_features = 'frame_000440.npy'
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    feats = torch.from_numpy(np.load(os.path.join(base_dir, folder_type, vae_features))).to(device)
+    print(feats.shape)
+    print(feats)
+    # min_val = feats.min().item()
+    # max_val = feats.max().item()
+    # print(min_val)
+    # print(max_val)
+    print('#############')
+    # Read a PIL image 
+    image = Image.open(os.path.join(base_dir, img_folder_type, filename)) 
+    
+    # Define a transform to convert PIL  image to a Torch tensor 
+    transform = transforms.Compose([ 
+        transforms.PILToTensor() 
+    ])
+
+    # Define the transformations
+    preprocess = transforms.Compose([
+        transforms.Resize(512, interpolation=Image.BILINEAR),
+        transforms.CenterCrop(512),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    # transform = transforms.PILToTensor() 
+    # Convert the PIL image to Torch tensor 
+    img_tensor = preprocess(image).to(device).unsqueeze(0)
+    print(img_tensor.shape) 
+    Dino = DINO().to(device)
+    dino_feats = Dino(img_tensor)
+    print(dino_feats)
+
+    # Example usage
+    save_tensor_as_image(dino_feats, 'output_dino_image.png')
+
+    save_tensor_as_image(feats, 'output_image.png')
+    # min_val = dino_feats.min().item()
+    # max_val = dino_feats.max().item()
+    # print(min_val)
+    # print(max_val)
+    # semantic_model.load_state_dict(torch.load('dinov2_vitl14_pretrain.pth'))
